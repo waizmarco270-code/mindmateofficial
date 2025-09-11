61c6703c0fc4490bd79362790b461e62ac3dc79a
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-09-11 09:55:06 +0000

    TypeError: Failed to fetch     at createFetch (webpack-internal:///(app-

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,10 @@
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-// Define routes that should be publicly accessible
-const isPublicRoute = createRouteMatcher([
-    '/',
-    '/api/trpc/(.*)', // Allow API calls
-]);
-
-// Define routes that should be protected and require authentication
-const isProtectedRoute = createRouteMatcher([
-    '/dashboard(.*)',
-    '/waizmarcoadmin(.*)',
-]);
-
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
-    auth().protect(); // Protect the protected routes
-  }
+export default clerkMiddleware({
+  publicRoutes: ["/"],
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
