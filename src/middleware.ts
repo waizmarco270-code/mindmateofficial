5ce327536066405d7e1c0a83f48f389fc9dc04e0
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-09-11 10:48:09 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-]);
-
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
-    auth().protect();
-  }
+export default clerkMiddleware({
+  publicRoutes: ['/'],
 });
 
 export const config = {
