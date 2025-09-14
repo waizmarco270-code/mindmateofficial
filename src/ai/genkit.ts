6682700c9177450d5cbc23ee10e1d76b4784334a
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-09-14 01:50:24 +0000

    Me sab code ko reset kar diya hu ab koi error nahi aye par jo changes Ki

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+  ],
   model: 'googleai/gemini-2.5-flash',
 });
