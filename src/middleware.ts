0120604b26623b87f83bc435a4a43751ae32d9b1
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-09-22 14:44:06 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,12 @@
 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/', '/dashboard(.*)']);
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+]);
 
-// The function passed to clerkMiddleware must be async
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
