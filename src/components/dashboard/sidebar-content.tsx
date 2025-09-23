a4fd118ae23e7f6a3924aed3d8e660d5572e4069
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-09-23 22:26:39 +0000

    Icon abhi bhi thik nahi hua yt or kharab hogaya kuch acha professional l

--- src/components/dashboard/sidebar-content.tsx
+++ src/components/dashboard/sidebar-content.tsx
@@ -80,7 +80,10 @@ const WhatsAppIcon = () => (
 )
 
 const YouTubeIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5"><path d="M2.5 17a24.12 24.12 0 0 1 0-10C2.5 6 2.5 4.5 3.5 3.5a2.44 2.44 0 0 1 2-1C7 2 12 2 12 2s5 0 6.5.5a2.44 2.44 0 0 1 2 1c1 1 1 2.5 1 3.5a24.12 24.12 0 0 1 0 10c0 1 0 2.5-1 3.5a2.44 2.44 0 0 1-2-1C17 22 12 22 12 22s-5 0-6.5-.5a2.44 2.44 0 0 1-2-1C2.5 19.5 2.5 18 2.5 17Z"></path><path d="m10 15 5-3-5-3z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5">
+        <path d="M2.5 17a24.12 24.12 0 0 1 0-10C2.5 6 2.5 4.5 3.5 3.5a2.44 2.44 0 0 1 2-1C7 2 12 2 12 2s5 0 6.5.5a2.44 2.44 0 0 1 2 1c1 1 1 2.5 1 3.5a24.12 24.12 0 0 1 0 10c0 1 0 2.5-1 3.5a2.44 2.44 0 0 1-2-1C17 22 12 22 12 22s-5 0-6.5-.5a2.44 2.44 0 0 1-2-1C2.5 19.5 2.5 18 2.5 17Z"></path>
+        <path d="m10 15 5-3-5-3z"></path>
+    </svg>
 )
 
 
