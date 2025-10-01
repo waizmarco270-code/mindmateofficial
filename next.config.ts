bc02eaff9963f461c29f089ad58d9f9ca83152df
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-10-01 06:58:58 +0000

    Chalo perfect !!! Ab suno mujhe tumse kuch puchna hai kya ham apna websi

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,17 @@
 
 import type {NextConfig} from 'next';
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -27,4 +39,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
