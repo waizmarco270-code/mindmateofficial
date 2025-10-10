843ab6913c608a778781fc649a44bdf1365fd1bb
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-10-10 11:23:38 +0000

    😡😭 abhi bhi thik nahi hora hai tumne ye wala MSG me kuch gadbad kar di

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,15 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_test_dHJ1c3R5LW1hY2FxdWUtMy5jbGVyay5hY2NvdW50cy5kZXYk',
+    CLERK_SECRET_KEY: 'sk_test_X5dOPfPsarLMVTfyKX0WM9eeA1U4YRX1RwLbN7qX5M',
+    NEXT_PUBLIC_FIREBASE_VAPID_KEY: 'BH5-Xy-Z3dC8zUnG71Dv4aMhOglYEf3a7P76z4Rge5A2cQDS5l9lliPPLTQQqKo4NfTTcOAWLK1K1Z-NF-3OwuQ',
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: "/sign-in",
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: "/sign-up",
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: "/dashboard",
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: "/dashboard",
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
