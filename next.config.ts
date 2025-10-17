6db2be5af5ae4737a0e16671a1529ca11616ae25
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-10-17 17:36:42 +0000

    Perfect ab suno hamra app me ye error arhaa hai app clerk se login hone

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
+  aggressiveFrontEndNavCaching: false, // Changed this to false
   reloadOnOnline: true,
   swcMinify: true,
   disable: process.env.NODE_ENV === "development",
