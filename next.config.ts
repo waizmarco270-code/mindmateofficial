aca8c06e48c5bb8967fbb6109c963e418d891125
Waiz Monazzum <waizmonazzum270@gmail.com>
2025-10-17 17:41:13 +0000

    Abhi bhi nahi khul raha hai ....aur ye bhi issue hai ek screenshot dekhi

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: false, // Changed this to false
+  aggressiveFrontEndNavCaching: false,
   reloadOnOnline: true,
   swcMinify: true,
   disable: process.env.NODE_ENV === "development",
@@ -17,13 +17,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_test_dHJ1c3R5LW1hY2FxdWUtMy5jbGVyay5hY2NvdW50cy5kZXYk',
-    CLERK_SECRET_KEY: 'sk_test_X5dOPfPsarLMVTfyKX0WM9eeA1U4YRX1RwLbN7qX5M',
     NEXT_PUBLIC_FIREBASE_VAPID_KEY: 'BH5-Xy-Z3dC8zUnG71Dv4aMhOglYEf3a7P76z4Rge5A2cQDS5l9lliPPLTQQqKo4NfTTcOAWLK1K1Z-NF-3OwuQ',
-    NEXT_PUBLIC_CLERK_SIGN_IN_URL: "/sign-in",
-    NEXT_PUBLIC_CLERK_SIGN_UP_URL: "/sign-up",
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: "/dashboard",
-    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: "/dashboard",
   },
   typescript: {
     ignoreBuildErrors: true,
